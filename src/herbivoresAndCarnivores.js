dd3b3a99af1df715b390867c880315660ca3a620
Ralfendeck25 <parischazhayan.21inf05@gmail.com>
2025-04-19 02:20:28 -0300

    Add new feature

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -10,7 +10,7 @@ class Animal {
   }
 
   die() {
-    Animal.alive = Animal.alive.filter(animal => animal !== this);
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
   }
 }
 
@@ -23,67 +23,28 @@ class Herbivore extends Animal {
   hide() {
     this.hidden = true;
   }
-class BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
-    this.name = name;
-    this.weight = weight;
-    this.coords = coords ? { ...coords } : { x: 0, y: 0 };
-    this.chipVersion = chipVersion;
-  }
-
-  goForward(step = 1) {
-    this.coords.y += step;
-  }
-
-  goBack(step = 1) {
-    this.coords.y -= step;
-  }
-
-  goRight(step = 1) {
-    this.coords.x += step;
-  }
-
-  goLeft(step = 1) {
-    this.coords.x -= step;
-  }
-
-  getInfo() {
-    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
-  }
 }
 
-class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
-    super(name, weight, coords, chipVersion);
-    // Garante que a coordenada z existe
-    if (this.coords.z === undefined) {
-      this.coords.z = 0;
+class Carnivore extends Animal {
+  bite(target) {
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+
+    if (target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+
+    if (target.health <= 0) {
+      target.die();
     }
   }
-
-  goUp(step = 1) {
-    this.coords.z += step;
-  }
-
-  goDown(step = 1) {
-    this.coords.z -= step;
-  }
 }
 
-class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null) {
-    super(name, weight, coords, chipVersion);
-    this.maxLoadWeight = maxLoadWeight;
-    this.currentLoad = currentLoad;
-  }
-
-  hookLoad(cargo) {
-    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
-      this.currentLoad = cargo;
-    }
-  }
-
-  unhookLoad() {
-    this.currentLoad = null;
-  }
-}
+module.exports = {
+  Animal,
+  Herbivore,
+  Carnivore,
+};
